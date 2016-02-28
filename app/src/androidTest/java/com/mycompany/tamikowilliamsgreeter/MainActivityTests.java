3929d9efe83bd54e426df05241af32226d2f997d
twilli58 <twilli58@my.westga.edu>
2016-02-28 00:34:42 -0500

    first commit

---
+++ app/src/androidTest/java/com/mycompany/tamikowilliamsgreeter/MainActivityTests.java
@@ -0,0 +1,54 @@
+package com.mycompany.tamikowilliamsgreeter;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by Miko on 2/27/2016.
+ */
+public class MainActivityTests extends
+        ActivityInstrumentationTestCase2<MainActivity> {
+
+    public MainActivityTests() {
+        super(MainActivity.class);
+    }
+    public void testActivityExists() {
+        MainActivity activity = getActivity();
+        assertNotNull(activity);
+    }
+    public void testGreet() {
+        MainActivity activity = getActivity();
+
+        final EditText nameEditText =
+                (EditText) activity.findViewById(R.id.greet_edit_text);
+
+        getInstrumentation().runOnMainSync(new Runnable() {
+            @Override
+            public void run() {
+                nameEditText.requestFocus();
+            }
+        });
+
+        getInstrumentation().waitForIdleSync();
+        getInstrumentation().sendStringSync("Jake");
+        getInstrumentation().waitForIdleSync();
+
+        // Tap "Greet" button
+        // ----------------------
+
+        Button greetButton =
+                (Button) activity.findViewById(R.id.greet_button);
+
+        TouchUtils.clickView(this, greetButton);
+
+        // Verify greet message
+        // ----------------------
+
+        TextView greetMessage = (TextView) activity.findViewById(R.id.message_text_view);
+        String actualText = greetMessage.getText().toString();
+        assertEquals("Hello, Jake!", actualText);
+    }
+}
