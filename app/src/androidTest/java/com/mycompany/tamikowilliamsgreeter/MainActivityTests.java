d6bc793c95e0e71293e95918e31229d498e609c0
twilli58 <twilli58@my.westga.edu>
2016-03-02 01:57:37 -0500

    adding testing updates to the files

--- app/src/androidTest/java/com/mycompany/tamikowilliamsgreeter/MainActivityTests.java
+++ app/src/androidTest/java/com/mycompany/tamikowilliamsgreeter/MainActivityTests.java
@@ -57,22 +57,62 @@ public class MainActivityTests extends
         assertEquals("Hello, Jake!", actualText);
 
     }
-   /** public void testReverseDisabled(){
+    public void testReverseDisabled(){
         MainActivity activity = getActivity();
 
         Button reverseButton =
                 (Button) activity.findViewById(R.id.reverse_button);
-        reverseButton.performClick();
         assertFalse(reverseButton.isEnabled());
 
     }
     public void testReverseEnabled(){
         MainActivity activity = getActivity();
-
         Button reverseButton =
                 (Button) activity.findViewById(R.id.reverse_button);
-        reverseButton.performClick();
+        Button greetButton =
+                (Button) activity.findViewById(R.id.greet_button);
+
+        TouchUtils.clickView(this, greetButton);
         assertTrue(reverseButton.isEnabled());
-    }*/
+
+    }
+    public void testTextIsReversed() {
+        MainActivity activity = getActivity();
+        Button greetButton =
+                (Button) activity.findViewById(R.id.greet_button);
+        Button reverseButton =
+                (Button) activity.findViewById(R.id.reverse_button);
+
+
+
+        final EditText nameEditText =
+                (EditText) activity.findViewById(R.id.greet_edit_text);
+
+        // Send string input value
+        getInstrumentation().runOnMainSync(new Runnable() {
+            @Override
+            public void run() {
+                nameEditText.requestFocus();
+            }
+        });
+
+        getInstrumentation().waitForIdleSync();
+        getInstrumentation().sendStringSync("Jake");
+        getInstrumentation().waitForIdleSync();
+
+        TouchUtils.clickView(this, greetButton);
+
+        TextView greetMessage = (TextView) activity.findViewById(R.id.message_text_view);
+        String actualText = greetMessage.getText().toString();
+        assertEquals("Hello, Jake!", actualText);
+
+        TouchUtils.clickView(this, reverseButton);
+
+        // Verify greet message
+        // ----------------------
+        String reverseText = greetMessage.getText().toString();
+        assertEquals("!ekaJ ,olleH", reverseText);
+
+    }
 }
 
